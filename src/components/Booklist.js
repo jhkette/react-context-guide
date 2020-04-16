cd76327165ce1c408fa06e6b21048afcc9cd37ff
khenryjoe <joseph.ketterer@gmail.com>
2020-04-16 13:02:55 +0100

    first lesson

---
+++ src/components/Booklist.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {ThemeContext} from '../contexts/ThemeContext';
+
+
+class BookList extends Component {
+    static contextType = ThemeContext
+    render() { 
+        const {isLightTheme, light, dark} = this.context;
+        const theme = isLightTheme ? light : dark;
+        return ( 
+            <div className='book-list' style={{background: theme.bg, color: theme.syntax}}>
+                <ul>
+                    <li style={{background: theme.ui}}>The way of kings</li>
+                    <li style={{background: theme.ui}}>The name fo the wind</li>
+                    <li style={{background: theme.ui}}>The final empire</li>
+
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default BookList;
